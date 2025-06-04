a57b25100ffafdf040ba1fef1e34c0212f173648
felipy <felipy81br@gmail.com>
2025-06-03 23:09:26 -0300

    fix: erro vercel deploy c

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // if(req.method !== 'POST') {
     //   return res.status(405).json({ error: 'Method not allowed.'})
     // }
-    return res.status(200).json({ method: req?.method, body: req?.body,error: 'deu tudo certo'})
+    // return res.status(200).json({ method: req?.method, body: req?.body,error: 'deu tudo certo'})
     const  {priceId} = req?.body;
   if(!priceId) {
     return res.status(400).json({ error: 'Price not found.'})
