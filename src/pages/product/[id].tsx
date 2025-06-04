827adfe24b1ffaffc7c0e8d700ac7baa04928547
felipy <felipy81br@gmail.com>
2025-06-03 22:46:07 -0300

    fix: erro vercel deploy

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -71,7 +71,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [
       {params: {id: 'prod_S0C6SDYxScDdme'}}
     ],
-    fallback: true
+    fallback: 'blocking'
   }
 }
 
