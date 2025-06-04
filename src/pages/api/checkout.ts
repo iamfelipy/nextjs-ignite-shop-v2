9cc4864380f3129d2e9322d2933d44e2f73fc351
felipy <felipy81br@gmail.com>
2025-06-03 23:16:27 -0300

    fix: erro vercel deploy c

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -6,14 +6,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // if(req.method !== 'POST') {
     //   return res.status(405).json({ error: 'Method not allowed.'})
     // }
-    return res.status(200).json({ method: req?.method, body: req?.body,error: 'deu tudo certo'})
     const  {priceId} = req?.body;
-  if(!priceId) {
-    return res.status(400).json({ error: 'Price not found.'})
-  }
-
-  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
-  const cancelUrl = `${process.env.NEXT_URL}/`
+    if(!priceId) {
+      return res.status(400).json({ error: 'Price not found.'})
+    }
+    
+    const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+    const cancelUrl = `${process.env.NEXT_URL}/`
+    return res.status(200).json({ method: req?.method, body: req?.body,error: 'deu tudo certo', successUrl, cancelUrl})
 
   const checkoutSession = await stripe.checkout.sessions.create({
     mode: 'payment',
@@ -26,7 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     ]
   })  
-  
+
   return res.status(201).json({
     checkoutUrl: checkoutSession.url,
   })
