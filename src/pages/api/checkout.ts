92cdd43a55f9b61ce94c23fba5377ad878b37f8f
felipy <felipy81br@gmail.com>
2025-06-03 23:04:12 -0300

    fix: erro vercel deploy c

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -2,11 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../lib/stripe";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const  {priceId} = req.body;
-
-  if(req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed.'})
-  }
+  
+  // if(req.method !== 'POST') {
+    //   return res.status(405).json({ error: 'Method not allowed.'})
+    // }
+    return res.status(200).json({ error: 'deu tudo certo'})
+    const  {priceId} = req.body;
   if(!priceId) {
     return res.status(400).json({ error: 'Price not found.'})
   }
